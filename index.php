e5186e94287683556a219a2cb9b974edf486afbf
System Administrator <root@MBPvonCristiano.fritz.box>
2025-08-02 10:29:20 +0200

    Simplify htaccess and restore original index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
-echo "PHP Test<br>";
-echo "Version: " . PHP_VERSION . "<br>";
-echo "Zeit: " . date('Y-m-d H:i:s');
+/**
+ * Metropol Portal - Root Index Bootstrap
+ * 
+ * @author 2Brands Media GmbH
+ */
+
+// Prüfen ob Installation abgeschlossen
+if (!file_exists(__DIR__ . '/.env')) {
+    // Noch nicht installiert - zum Installer
+    header('Location: install.php');
+    exit;
+}
+
+// Arbeitsverzeichnis auf public setzen
+if (file_exists(__DIR__ . '/public/index.php')) {
+    chdir(__DIR__ . '/public');
+    require_once __DIR__ . '/public/index.php';
+} else {
+    echo "Anwendung noch nicht vollständig installiert.";
+}
 ?>
